71d72e22f5bca708dd022053590aa71832da278f
Phenoo <descometusah@gmail.com>
2026-01-03 23:07:33 +0100

    shshshs 1121a

--- sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { artwork } from './artwork'
-import { event } from './event'
-import { service } from './service'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [artwork, event, service],
-}
